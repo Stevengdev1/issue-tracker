faa061472a67bb481f312464ebe5fb47b77a66d9
Stevengdev1 <stevendg922@gmail.com>
2024-06-05 12:57:57 -0400

    Built an API for creating issues (corrected code)

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
-import prisma from "@prisma/client";
+import prisma from "@/prisma/client";
 
 
 const createIssueSchema = z.object({
@@ -12,11 +12,11 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createIssueSchema.safeParse(body);
     if (!validation.success) 
-        return NextRequest.json(validation.error.errors, { status:4})
+        return NextResponse.json(validation.error.errors, { status:400})
 
     const newIssue  = await prisma.issue.create({
         data: { title: body.title, description: body.description}
-    })
+    });
 
     return NextResponse.json(newIssue, { status:201});
 }
--- prisma/client.ts
+++ prisma/client.ts
@@ -1,15 +1,36 @@
+// import { PrismaClient } from '@prisma/client'
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+
+// THE CODE BELOW IS THE EXACT CODE MOSH COPIED FROM PRISMA.IO 
+
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined
+  } 
+  
+  const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+  
+  export default prisma
+  
+  if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
